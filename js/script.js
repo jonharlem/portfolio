56370dcbb1ffea86f8194bd1429d8e5c065e9f3a
Jon Harlem <jtharlem@gmail.com>
2016-03-18 17:08:05 -0700

    css updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-script.js
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.row').hover(function(){
+    	(this).removeClass(".col-sm-3");
+    	// .addClass(".col-sm-12");
+    });
+});
